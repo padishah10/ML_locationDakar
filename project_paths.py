0b331005ffe74f8152bc2cd3c3803c8d27e3528f
PADI <padidiallo524@gmail.com>
2026-03-30 18:33:29 +0000

    Update project_paths.py

--- project_paths.py
+++ project_paths.py
@@ -1,6 +1,6 @@
-﻿from pathlib import Path
+from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent
 DATA_DIR = PROJECT_ROOT / "Fichiers CSV"
 MODELS_DIR = PROJECT_ROOT / "Modeles"
 METRICS_DIR = PROJECT_ROOT / "Metriques"
